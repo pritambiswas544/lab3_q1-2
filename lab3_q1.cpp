6eba89ddf1b10bac95b0feec474c59f1e116e8a9
pritambiswas544 <42238214+pritambiswas544@users.noreply.github.com>
2018-08-17 05:54:36 +0000

    v 3.0

---
+++ lab3_q1.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+int main(){
+int a=1;
+float b=2.303;
+double c=5.61;
+char d=2;
+bool e=1;
+//Variables size
+
+cout<< sizeof(b)<<endl;
+cout<< sizeof(a)<<endl;
+cout<< sizeof(c)<<endl;
+cout<< sizeof(d)<<endl;
+cout<< sizeof(e)<<endl;
+return 5;
+
+}
+
+
---
+++ lab3_q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+int a=40;
+int b=21;
+int c=a+b;
+cout << c<<endl;
+int d=a-b;
+cout << d<<endl;
+int e=a*b;
+cout << e<<endl;
+int f=a/b;
+cout << f<<endl;
+float g=a%b;
+cout << g<<endl;
+return 0;
+}
